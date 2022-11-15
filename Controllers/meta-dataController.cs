648785066b908d3741b0738048467e79f230d5a6
SanthoshKumar-2002 <manojsanthoshmanojsanthosh7@gmail.com>
2022-11-15 15:49:49 +0530

    Initial commit

---
+++ Controllers/meta-dataController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi2.Contracts;
+
+namespace WebApi2.Controllers
+{
+    [Route("api/[controller]/")]
+    [ApiController]
+    public class meta_dataController : ControllerBase
+    {
+        private readonly IUserService userContract;
+
+        public meta_dataController(IUserService userContract)
+        {
+            this.userContract = userContract;
+        }
+        /// <summary>
+        /// for getting the meta data
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        // meta data
+        [HttpGet]
+        [Route("[Action]/{key}")]
+        [Authorize]
+        public IActionResult ref_Set(int key)
+        {
+            string sub = HttpContext?.User.Claims.FirstOrDefault(c => c.Type == System.Security.Claims.ClaimTypes.NameIdentifier).Value;
+            if (userContract.usercheck(sub))
+                return Ok(userContract.GetMetadata(key));
+            return Unauthorized();  
+        }
+    }
+}
